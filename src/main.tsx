bcbaa5c5a6503f3f4821928c110d3d5b68b247f1
Christopher <christopherkarlsson95@gmail.com>
2022-04-21 22:13:33 +0200

    Routes added

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { createGlobalStyle } from 'styled-components';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import JobPage from './JobPage';
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -17,7 +19,14 @@ const GlobalStyle = createGlobalStyle`
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyle />
-    <App />
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/stats" element={<></>} />
+        <Route path="job/:id" element={<JobPage />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
