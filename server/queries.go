d4a74ff5a9579f657d6a2c05a6d9b91f436c7959
Christopher <christopherkarlsson95@gmail.com>
2022-04-23 20:35:03 +0200

    Filter functions added to frontend

--- server/queries.go
+++ server/queries.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func queries() {
+	fullFilterQuery := "SELECT JobId, Title, Occupation, Municipality, PublishedDate, Keywords FROM job WHERE INSTR(Keywords, ?) > 0"
+
 	titleSearch := "\"Select JobId, Title, CompanyName, Municipality, PublishedDate, Keywords FROM job WHERE INSTR(Keywords, ?) > 0 LIMIT 100;\", title.Search"
 
 	keywordSearch := "\"Select JobId, Title, CompanyName, Municipality, PublishedDate, Keywords FROM job WHERE INSTR(Keywords, ?) > 0 LIMIT 100;\", municipality.Search"
